684628f4dc1139dd6553b5f2a1fa6f0f90d8f71b
badrca <bghazouan9@gmail.com>
2018-11-22 21:10:16 +0100

    class decorators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,25 @@
 import { Component } from '@angular/core';
 
-function Console(target,name,descriptor) {
-  console.log("print in console :  " + target,name,descriptor );
-  //console.log("print in console :  " + target+ "-- " + name + " -- " + descriptor);
-  
 
-  // store the original function (descriptor.value is a function)
+function logging(className) {
+  console.log(className)
 
-  const original  = descriptor.value
-  // do some manipulation with descriptor.value function
-
-  original();  // console(hey there)
-
-  descriptor.value = function(...args) {
-    console.log("descriptor is hacked" )
-    const result = original(...args)
-    console.log("result is ", result)
-    return result
+  return (...args) =>{
+    console.log("arguments passed : ", args)
+    return new className(...args)
   }
-  // return the new descriptor
-      return descriptor;
- 
 }
 
-function log(message) {
-  // access the "metadata" message
-  console.log(message);
-  // return a function closure, which
-  // is passed the class as `target`
-  return function(target) {
-    console.log('Our decorated class', target);
-  };
+@logging
+ class ExampleClass {
+
+  constructor(...args) {
+    console.log("constructor fired");
+     }
 }
 
-let myObj = {
-  myPropOne: 1,
-  myPropTwo: 2
-};
+const myclass = new ExampleClass(4,5);
+
 
 
 @Component({
@@ -47,16 +30,5 @@ let myObj = {
 export class AppComponent {
   title = 'introAngular from ghazouan';
   
-   
-
-  constructor() {
-   // console.log("constructor from AppComponent : " + Object.getOwnPropertyDescriptor(myObj,myObj.myPropOne));
-    console.log("result from constructor is ", this.asimplemethod(5,3));
-  }
-
-    @Console
-    asimplemethod(a,b) {
-      return a*b
-    
-  }
+ 
 }
