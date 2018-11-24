f072c09a1798068d08581cc1c7407374fe745f20
badrca <bghazouan9@gmail.com>
2018-11-24 02:59:42 +0100

    structural directive

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -11,33 +11,42 @@ export class HelloComponent implements OnInit {
   othervar = "whatever";
   mydisabledvar = false;
 
-  constructor() { 
-    setInterval(()=>{
-      //this.myvar  = Math.random().toString();   
-      //console.log("myvar  = " +  this.myvar);
-      /*    documentation
-        myvar is one way binded variable, it's value from component.ts(javascript) to html(view), not vice versa
-        mydisabledvar is also one way binded, but from view (html) to component.ts(javscript)
-      */
-    },1000)
-  }
-  /*
-    to use two way data binding, we add  [(ngModel)]  in html input, 
-    add FormsModule from '@angular/forms' to import array in app.module.ts
-  */
-
+  constructor() {  }
+ 
   ngOnInit() {
   
     console.log("ngOnInit is called on hello component")
   }
-  callMyFunction() {
-    this.mydisabledvar = !this.mydisabledvar;
-    console.log("function called");
-  }
 
-  updateInput(e) {
-    console.log(e.target.value);
-    this.othervar = e.target.value;
-  }
+  records = [
+    {
+      name:"badr",
+      option : "info"
+    },
+    {
+      name : "omar",
+      option : "data info"
+    },
+    {
+      name : "abcd",
+      option : "xye" 
+    },
+    {
+      name:"badr",
+      option : "info"
+    },
+    {
+      name : "omar",
+      option : "data info"
+    },
+    {
+      name : "abcd",
+      option : "xye" 
+    }
+  ]
   
+  months = ["January", "February", "March", "April",
+  "May", "June", "July", "August", "September",
+  "October", "November", "December"];
+isavailable = false;   //variable is set to true
 }
